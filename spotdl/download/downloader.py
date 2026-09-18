cc0ea7f7182802b0dad8c4496b779b77f32fa3b8
Ezra Tock <etock@u.rochester.edu>
2026-09-18 16:25:18 -0700

    skip MusixMatch instead of raise on missing credentials

--- spotdl/download/downloader.py
+++ spotdl/download/downloader.py
@@ -196,12 +196,9 @@ class Downloader:
                 musixmatch_email = self.settings.get("musixmatch_email")
                 musixmatch_password = self.settings.get("musixmatch_password")
                 if not musixmatch_email or not musixmatch_password:
-                    raise DownloaderError(
-                        "MusixMatch email/password not found in settings"
-                    )
-                self.lyrics_providers.append(
-                    MusixMatch(musixmatch_email, musixmatch_password)
-                )
+                    logger.debug("MusixMatch email/password not found in settings. Skipping MusixMatch.")
+                    continue
+                self.lyrics_providers.append(MusixMatch(musixmatch_email, musixmatch_password))
             else:
                 self.lyrics_providers.append(lyrics_class())
 
